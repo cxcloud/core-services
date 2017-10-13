3421c940f2f636a5873ba29158f4bb3474ce0f46
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-13 12:28:28 +0300

    Add product types

---
+++ src/sdk/types/products.ts
@@ -0,0 +1,37 @@
+import { Attribute, Image, LocalizedString, Price, Reference } from './common';
+import { PaginatedResult } from './result';
+
+export interface CategoryOrderHints {}
+
+export interface Variant {
+  id: number;
+  sku: string;
+  prices: Price[];
+  images: Image[];
+  attributes: Attribute[];
+  assets: any[];
+}
+
+export interface SearchKeywords {}
+
+export interface Product {
+  id: string;
+  version: number;
+  productType: Reference;
+  name: LocalizedString;
+  categories: Reference[];
+  categoryOrderHints: CategoryOrderHints;
+  slug: LocalizedString;
+  masterVariant: Variant;
+  variants: Variant[];
+  searchKeywords: SearchKeywords;
+  hasStagedChanges: boolean;
+  published: boolean;
+  taxCategory: Reference;
+  createdAt: Date;
+  lastModifiedAt: Date;
+}
+
+export interface PaginatedProductResult extends PaginatedResult {
+  results: Product[];
+}
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,4 @@
 import { Products } from './';
 
-Products.fetchAll().then(res => console.log(res.results));
+// Products.getProductsBySearchQuery('New test product').then(res => console.log(res.results));
+Products.getProductById('5e00f2b3-d098-4b6b-82f8-895d91be9c79').then(res => console.log(JSON.stringify(res)));
