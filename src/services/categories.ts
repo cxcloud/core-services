bfb62cfde151090e3812a3adcb5811af5a173a82
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-13 14:17:01 +0300

    Add category service

---
+++ src/services/categories.ts
@@ -0,0 +1,47 @@
+import { execute, methods, services } from '../sdk';
+import { Category, PaginatedCategoryResult } from '../sdk/types/categories';
+
+interface CategoryMap {
+  [id: string]: Category;
+}
+
+function nestify(categories: Category[]): Category[] {
+  const rootCategories: Category[] = [];
+  const catMap = categories.reduce(
+    (map: CategoryMap, cat: Category) => {
+      map[cat.id] = cat;
+      return map;
+    },
+    {} as CategoryMap
+  );
+
+  categories.forEach(cat => {
+    if (cat.parent) {
+      const parent = catMap[cat.parent.id];
+      parent.subCategories = parent.subCategories || [];
+      parent.subCategories.push(cat);
+    } else {
+      rootCategories.push(cat);
+    }
+  });
+
+  return rootCategories;
+}
+
+export namespace Categories {
+  export function fetchAll(flat = false): Promise<Category> {
+    // @TODO: need to check if there's more pages
+    const query = services.categories.perPage(500);
+    return execute(query, methods.GET).then((res: PaginatedCategoryResult) => {
+      if (flat === true) {
+        return res.results;
+      }
+      return nestify(res.results);
+    });
+  }
+
+  export function getById(categoryId: string): Promise<Category> {
+    const query = services.categories.byId(categoryId);
+    return execute(query, methods.GET);
+  }
+}
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,5 @@
-import { Products } from './';
+import { Categories } from './';
 
 // Products.getProductsBySearchQuery('New test product').then(res => console.log(res.results));
-Products.getProductById('5e00f2b3-d098-4b6b-82f8-895d91be9c79').then(res => console.log(JSON.stringify(res)));
+// Products.findById('5e00f2b3-d098-4b6b-82f8-895d91be9c79').then(res => console.log(JSON.stringify(res)));
+Categories.fetchAll().then(res => console.log(JSON.stringify(res, undefined, 2)));
