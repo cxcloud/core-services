023c70e4d04bb2e6928956239b28748c806f2dd9
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-13 12:10:22 +0300

    Add product search functions

--- src/services/products.ts
+++ src/services/products.ts
@@ -2,7 +2,21 @@ import { execute, methods, services } from '../sdk';
 import { PaginatedResult } from '../sdk/types/result';
 
 export namespace Products {
-  export function fetchAll(): Promise<PaginatedResult> {
-    return execute(services.products.perPage(20), methods.GET);
+  export function getProductsByCategoryId(categoryId: string): Promise<PaginatedResult> {
+    const query = services.productProjectionsSearch.filter(`categories.id:subtree("${categoryId}")`).perPage(20);
+    return execute(query, methods.GET);
+  }
+
+  export function getProductsBySearchQuery(q: string, language = 'en'): Promise<PaginatedResult> {
+    const query = services.productProjectionsSearch
+      .text(q, language)
+      .fuzzy(true)
+      .perPage(20);
+    return execute(query, methods.GET);
+  }
+
+  export function getProductById(productId: string): Promise<any> {
+    const query = services.products.byId(productId);
+    return execute(query, methods.GET);
   }
 }
