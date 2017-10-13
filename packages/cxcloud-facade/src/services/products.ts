63eef2169f0016f3c64eb3bbed686a2255c27279
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-13 11:57:44 +0300

    Load products

--- packages/cxcloud-facade/src/services/products.ts
+++ packages/cxcloud-facade/src/services/products.ts
@@ -1,5 +1,8 @@
+import { execute, methods, services } from '../sdk';
+import { PaginatedResult } from '../sdk/types/result';
+
 export namespace Products {
-  export function fetchAll() {
-    console.log('bae');
+  export function fetchAll(): Promise<PaginatedResult> {
+    return execute(services.products.perPage(20), methods.GET);
   }
 }
