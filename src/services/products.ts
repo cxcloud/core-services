799909c1dc5c771aeaa72e7fae7a0d4b0f2d4e7d
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-13 14:16:54 +0300

    Rename product methods

--- src/services/products.ts
+++ src/services/products.ts
@@ -2,12 +2,12 @@ import { execute, methods, services } from '../sdk';
 import { PaginatedProductResult, Product } from '../sdk/types/products';
 
 export namespace Products {
-  export function getProductsByCategoryId(categoryId: string): Promise<PaginatedProductResult> {
+  export function findByCategoryId(categoryId: string): Promise<PaginatedProductResult> {
     const query = services.productProjectionsSearch.filter(`categories.id:subtree("${categoryId}")`).perPage(20);
     return execute(query, methods.GET);
   }
 
-  export function getProductsBySearchQuery(q: string, language = 'en'): Promise<PaginatedProductResult> {
+  export function findBySearchQuery(q: string, language = 'en'): Promise<PaginatedProductResult> {
     const query = services.productProjectionsSearch
       .text(q, language)
       .fuzzy(true)
@@ -15,7 +15,7 @@ export namespace Products {
     return execute(query, methods.GET);
   }
 
-  export function getProductById(productId: string): Promise<Product> {
+  export function findById(productId: string): Promise<Product> {
     const query = services.productProjections.byId(productId);
     return execute(query, methods.GET);
   }
