7434b636c958147af3645ee49f11a05b8c0e1a3f
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-13 14:17:01 +0300

    Add category service

--- packages/cxcloud-facade/src/index.ts
+++ packages/cxcloud-facade/src/index.ts
@@ -1,1 +1,2 @@
 export * from './services/products';
+export * from './services/categories';
---
+++ packages/cxcloud-facade/src/sdk/types/categories.ts
@@ -0,0 +1,22 @@
+import { LocalizedString, Reference } from './common';
+import { PaginatedResult } from './result';
+
+export interface Category {
+  id: string;
+  version: number;
+  name: LocalizedString;
+  slug: LocalizedString;
+  description: LocalizedString;
+  ancestors: Reference[];
+  orderHint: string;
+  createdAt: Date;
+  lastModifiedAt: Date;
+  assets: any[];
+  lastMessageSequenceNumber: number;
+  parent: Reference;
+  subCategories?: Category[];
+}
+
+export interface PaginatedCategoryResult extends PaginatedResult {
+  results: Category[];
+}
