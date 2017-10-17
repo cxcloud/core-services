d0252f7faa78853c905a03b3fb7c2ce94f9bb056
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-17 12:38:52 +0300

    Add generic actions to carts

--- packages/cxcloud-facade/src/services/carts.ts
+++ packages/cxcloud-facade/src/services/carts.ts
@@ -6,6 +6,11 @@ import { Customer } from '../sdk/types/customers';
 import { getCustomerCurrency, getCustomerShippingAddress } from '../tools/customers';
 
 export namespace Carts {
+  export interface ICartAction {
+    action: string;
+    [key: string]: any;
+  }
+
   export interface IAddLineItem {
     productId: string;
     variantId: number;
@@ -37,6 +42,20 @@ export namespace Carts {
     return execute(services.carts.byId(cartId), methods.GET);
   }
 
+  export function updateByActions(
+    cartId: string,
+    cartVersion: number,
+    actions: ICartAction | ICartAction[]
+  ): Promise<Cart> {
+    if (!Array.isArray(actions)) {
+      actions = [actions];
+    }
+    return execute(services.carts.byId(cartId), methods.POST, {
+      version: cartVersion,
+      actions
+    });
+  }
+
   export function addLineItems(
     cartId: string,
     cartVersion: number,
@@ -45,13 +64,14 @@ export namespace Carts {
     if (!Array.isArray(lineItems)) {
       lineItems = [lineItems];
     }
-    return execute(services.carts.byId(cartId), methods.POST, {
-      version: cartVersion,
-      actions: lineItems.map(li => ({
+    return updateByActions(
+      cartId,
+      cartVersion,
+      lineItems.map(li => ({
         action: 'addLineItem',
         ...li
       }))
-    });
+    );
   }
 
   export function changeLineItemQuantity(
@@ -59,15 +79,12 @@ export namespace Carts {
     cartVersion: number,
     action: IChangeLineItemQuantity
   ): Promise<Cart> {
-    return execute(services.carts.byId(cartId), methods.POST, {
-      version: cartVersion,
-      actions: [
-        {
-          action: 'changeLineItemQuantity',
-          ...action
-        }
-      ]
-    });
+    return updateByActions(cartId, cartVersion, [
+      {
+        action: 'changeLineItemQuantity',
+        ...action
+      }
+    ]);
   }
 
   export function removeLineItem(
@@ -75,14 +92,11 @@ export namespace Carts {
     cartVersion: number,
     lineItemId: string
   ): Promise<Cart> {
-    return execute(services.carts.byId(cartId), methods.POST, {
-      version: cartVersion,
-      actions: [
-        {
-          action: 'removeLineItem',
-          lineItemId
-        }
-      ]
-    });
+    return updateByActions(cartId, cartVersion, [
+      {
+        action: 'removeLineItem',
+        lineItemId
+      }
+    ]);
   }
 }
