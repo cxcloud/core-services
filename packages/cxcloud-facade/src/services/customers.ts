a17fcb51ba1a088e3ec60760aaa305b9824f9b61
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-30 14:26:16 +0200

    Improve customer login method

--- packages/cxcloud-facade/src/services/customers.ts
+++ packages/cxcloud-facade/src/services/customers.ts
@@ -1,27 +1,58 @@
-import { methods, sdkConfig, authenticatedFormRequest } from '../sdk';
+import {
+  methods,
+  sdkConfig,
+  authenticatedFormRequest,
+  clientExecute,
+  services
+} from '../sdk';
+import {
+  CustomerSignInResult,
+  CustomerTokenResponse,
+  SignInResult,
+  Customer
+} from '../sdk/types/customers';
 import { stringify } from 'querystring';
+import omit = require('lodash/omit');
+
+const userScopes = [
+  'manage_my_orders',
+  'manage_my_profile',
+  'manage_my_shopping_lists',
+  'view_products',
+  'manage_my_profile',
+  'manage_my_payments'
+];
 
 export namespace Customers {
-  export function login(username: string, password: string) {
-    const scopes = [
-      'manage_my_orders',
-      'manage_my_profile',
-      'manage_my_shopping_lists',
-      'view_products',
-      'manage_my_profile',
-      'manage_my_payments'
-    ]
-      .map(scope => `${scope}:${sdkConfig.projectKey}`)
-      .join(' ');
-    return authenticatedFormRequest({
-      uri: `${sdkConfig.authHost}/oauth/${sdkConfig.projectKey}/customers/token`,
+  export function login(
+    username: string,
+    password: string
+  ): Promise<SignInResult> {
+    return clientExecute<CustomerSignInResult>({
+      uri: services.login.build(),
       method: methods.POST,
-      body: stringify({
-        grant_type: 'password',
-        username,
-        password,
-        scope: scopes
-      })
+      body: {
+        email: username,
+        password
+      }
+    }).then(loginResult => {
+      const scopes = userScopes
+        .map(scope => `${scope}:${sdkConfig.projectKey}`)
+        .join(' ');
+      return authenticatedFormRequest<CustomerTokenResponse>({
+        uri: `${sdkConfig.authHost}/oauth/${sdkConfig.projectKey}/customers/token`,
+        method: methods.POST,
+        body: stringify({
+          grant_type: 'password',
+          username,
+          password,
+          scope: scopes
+        })
+      }).then(token => ({
+        token,
+        customer: omit(loginResult.customer, ['password']) as Customer,
+        cart: loginResult.cart || null
+      }));
     });
   }
 }
