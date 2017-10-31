4b676c0ebf773334b5766d18b2fd830ccbd86d67
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-31 13:59:18 +0200

    Add encryption to use tokens, customer lookup

--- packages/cxcloud-facade/src/services/customers.ts
+++ packages/cxcloud-facade/src/services/customers.ts
@@ -7,12 +7,18 @@ import {
 } from '../sdk';
 import {
   CustomerSignInResult,
-  CustomerTokenResponse,
+  OAuthToken,
   SignInResult,
   Customer
 } from '../sdk/types/customers';
+import { getCustomerIdFromToken, encryptTokenResponse } from '../tools/crypto';
 import { stringify } from 'querystring';
+import * as Cache from 'node-cache';
 import omit = require('lodash/omit');
+
+const customerCache = new Cache({
+  stdTTL: 60 * 15 // 15 mins
+});
 
 const userScopes = [
   'manage_my_orders',
@@ -39,7 +45,7 @@ export namespace Customers {
       const scopes = userScopes
         .map(scope => `${scope}:${sdkConfig.projectKey}`)
         .join(' ');
-      return authenticatedFormRequest<CustomerTokenResponse>({
+      return authenticatedFormRequest<OAuthToken>({
         uri: `${sdkConfig.authHost}/oauth/${sdkConfig.projectKey}/customers/token`,
         method: methods.POST,
         body: stringify({
@@ -49,10 +55,32 @@ export namespace Customers {
           scope: scopes
         })
       }).then(token => ({
-        token,
+        token: encryptTokenResponse(token, loginResult.customer),
         customer: omit(loginResult.customer, ['password']) as Customer,
         cart: loginResult.cart || null
       }));
+    });
+  }
+
+  export function findById(customerId: string): Promise<Customer> {
+    return clientExecute<Customer>({
+      uri: services.customers.byId(customerId).build(),
+      method: methods.GET
+    });
+  }
+
+  export async function findByAuthToken(token: string): Promise<Customer> {
+    const cached = customerCache.get<Customer>(token);
+    if (cached) {
+      return cached;
+    }
+    const customerId = getCustomerIdFromToken(token);
+    if (!customerId) {
+      throw new Error('Invalid token provided');
+    }
+    return findById(customerId).then(customer => {
+      customerCache.set(token, customer);
+      return customer;
     });
   }
 }
---
+++ packages/cxcloud-facade/src/tools/crypto.ts
@@ -0,0 +1,52 @@
+import { Customer, OAuthToken } from '../sdk/types/customers';
+import * as crypto from 'crypto';
+
+const ENCRYPTION_KEY = 'wY[Ax)FC0AlQjruD$9J_tO3U+YiMZyL1'; // @TODO: REMOVE
+const IV_LENGTH = 16;
+const TEXT_SEPARATOR = ':';
+
+export function encrypt(text: string): string {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv(
+    'aes-256-cbc',
+    new Buffer(ENCRYPTION_KEY),
+    iv
+  );
+
+  let encrypted = cipher.update(new Buffer(text));
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return iv.toString('base64') + TEXT_SEPARATOR + encrypted.toString('base64');
+}
+
+export function decrypt(text: string): string {
+  const [ivStr, encryptedStr] = text.split(TEXT_SEPARATOR);
+  const iv = new Buffer(ivStr, 'base64');
+  const encryptedText = new Buffer(encryptedStr, 'base64');
+  const decipher = crypto.createDecipheriv(
+    'aes-256-cbc',
+    new Buffer(ENCRYPTION_KEY),
+    iv
+  );
+
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return decrypted.toString();
+}
+
+export function getCustomerIdFromToken(token: string): string {
+  const decrypted = decrypt(token);
+  const [customerId] = decrypted.split(':');
+  return customerId;
+}
+
+export function encryptTokenResponse(
+  token: OAuthToken,
+  customer: Customer
+): OAuthToken {
+  return {
+    ...token,
+    access_token: encrypt(`${customer.id}:${token.access_token}`)
+  };
+}
