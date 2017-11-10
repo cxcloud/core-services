e6b6b1de15eb42777cfe366dd832a9db9f495dcf
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-11-10 11:13:32 +0200

    Add sdk types

---
+++ packages/ct-types/sdk.ts
@@ -0,0 +1,30 @@
+export interface PaginatedResult {
+  limit: number;
+  offset: number;
+  count: number;
+  total: number;
+  results: any[];
+}
+
+export type ClientRequest = {
+  uri: string;
+  method: string;
+  body?: string | Object;
+  token?: string;
+  headers?: {
+    [key: string]: string;
+  };
+};
+
+export type ClientCredentials = {
+  clientId: string;
+  clientSecret: string;
+};
+
+export type SdkConfig = {
+  projectKey: string;
+  admin: ClientCredentials;
+  user: ClientCredentials;
+  authHost: string;
+  apiHost: string;
+};
