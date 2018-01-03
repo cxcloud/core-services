dc8e1645c7dfcdf8da4325e2815f0874615f9bad
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-01-03 16:02:20 +0200

    Convert pool to a class

--- packages/cxcloud-process-engine/src/pool.ts
+++ packages/cxcloud-process-engine/src/pool.ts
@@ -1,16 +1,27 @@
 import { QueueProcessor } from './queue';
 
-export function createQueuePool(pool: QueueProcessor[]) {
-  return {
-    start() {
-      pool.forEach(processor => processor.start());
-    },
-    findByName(name: string): QueueProcessor {
-      const processor = pool.find(poolItem => poolItem.options.name === name);
-      if (!processor) {
-        throw new Error('Processor has not been found');
-      }
-      return processor;
+export class QueuePool {
+  private __processors: QueueProcessor[];
+
+  constructor(processors: QueueProcessor[]) {
+    this.__processors = processors;
+  }
+
+  start() {
+    this.__processors.forEach(processor => processor.start());
+  }
+
+  findByName(name: string): QueueProcessor {
+    const processor = this.__processors.find(
+      poolItem => poolItem.options.name === name
+    );
+    if (!processor) {
+      throw new Error('Processor has not been found');
     }
-  };
+    return processor;
+  }
+}
+
+export function createQueuePool(processors: QueueProcessor[]): QueuePool {
+  return new QueuePool(processors);
 }
