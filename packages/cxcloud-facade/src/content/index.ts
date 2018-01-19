8bf9676abb2c04fb4405e8199d6aab9fc6dbd0a1
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-01-19 14:30:54 +0200

    Changes to contentful client

--- packages/cxcloud-facade/src/content/index.ts
+++ packages/cxcloud-facade/src/content/index.ts
@@ -1,21 +1,23 @@
 import { Entry, EntryCollection, Space } from 'contentful';
-import { client } from './sdk';
+import { getClient } from './sdk';
 
 export namespace Content {
   export function getEntry(entryId: string, query?: any): Promise<Entry<any>> {
-    return client.getEntries({ 'sys.id': entryId, ...query }).then(res => {
-      if (res.total > 0) {
-        return res.items[0];
-      }
-      throw new Error(`Entry with ID: ${entryId} not found.`);
-    });
+    return getClient()
+      .getEntries({ 'sys.id': entryId, ...query })
+      .then((res: any) => {
+        if (res.total > 0) {
+          return res.items[0];
+        }
+        throw new Error(`Entry with ID: ${entryId} not found.`);
+      });
   }
 
   export function getEntries(query: any): Promise<EntryCollection<any>> {
-    return client.getEntries(query);
+    return getClient().getEntries(query);
   }
 
   export function getSpace(): Promise<Space> {
-    return client.getSpace();
+    return getClient().getSpace();
   }
 }
