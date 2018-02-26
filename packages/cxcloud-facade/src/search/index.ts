415ef8b53826a5e32e29026af924e961a2d1510a
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-02-26 14:58:58 +0200

    Export type

--- packages/cxcloud-facade/src/search/index.ts
+++ packages/cxcloud-facade/src/search/index.ts
@@ -2,6 +2,7 @@ import { AlgoliaQueryParameters, AlgoliaResponse } from 'algoliasearch';
 import { getClient } from './sdk';
 
 export namespace Search {
+  export type QueryParams = AlgoliaQueryParameters;
   export function searchIndex(
     indexName: string,
     params: AlgoliaQueryParameters
