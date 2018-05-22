f253df21ac05f35b32d2a0821552664dcf9d1285
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-05-22 12:28:35 +0300

    Refactor types

---
+++ packages/cxcloud-facade/src/auth/sdk/types.ts
@@ -0,0 +1,35 @@
+export type Status = 'SUCCESS';
+
+export interface RegisterResult {
+  username: string;
+}
+
+export interface CodeDeliveryResult {
+  CodeDeliveryDetails: {
+    Destination: string;
+    DeliveryMedium: string;
+    AttributeName: string;
+  };
+}
+
+export interface LoginSuccessResult {
+  refreshToken: string;
+  accessToken: string;
+  accessTokenExpiresAt: string;
+  idToken: string;
+  idTokenExpiresAt: string;
+}
+
+export interface LoginNextStepResult {
+  nextStep: 'MFA_AUTH' | 'NEW_PASSWORD_REQUIRED';
+  loginSession: string;
+}
+
+export interface AttributesHash {
+  [key: string]: string;
+}
+
+export interface CognitoAttribute {
+  Name: string;
+  Value: string;
+}
