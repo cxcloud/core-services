29b4b7f03041f6470a926a6574c367e2d88c229d
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-08-12 15:37:27 +0300

    Move content to its own module

--- packages/cxcloud-commerce/src/index.ts
+++ packages/cxcloud-commerce/src/index.ts
@@ -1,6 +1,4 @@
 import * as commerceServices from './commerce';
 
-export { Content } from './content';
 export { Search } from './search';
-export { Auth } from './auth';
 export const Commerce = commerceServices;
---
+++ packages/cxcloud-content/src/index.ts
@@ -0,0 +1,35 @@
+import {
+  Entry,
+  EntryCollection,
+  Space,
+  Asset,
+  ContentfulCollection
+} from 'contentful';
+import { getClient } from './sdk';
+
+export function getEntry(entryId: string, query?: any): Promise<Entry<any>> {
+  return getClient()
+    .getEntries({ 'sys.id': entryId, ...query })
+    .then((res: any) => {
+      if (res.total > 0) {
+        return res.items[0];
+      }
+      throw new Error(`Entry with ID: ${entryId} not found.`);
+    });
+}
+
+export function getEntries(query: any): Promise<EntryCollection<any>> {
+  return getClient().getEntries(query);
+}
+
+export function getSpace(): Promise<Space> {
+  return getClient().getSpace();
+}
+
+export function getAsset(id: string): Promise<Asset> {
+  return getClient().getAsset(id);
+}
+
+export function getAssets(query: any): Promise<ContentfulCollection<Asset>> {
+  return getClient().getAssets(query);
+}
