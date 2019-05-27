517a1bcaae1e5e250449145d7b1f42984edf2809
Sallar Kaboli <sallar@kaboli.org>
2019-05-27 12:58:15 +0300

    Add state draft

--- packages/cxcloud-ct-types/states.d.ts
+++ packages/cxcloud-ct-types/states.d.ts
@@ -1,11 +1,20 @@
 import { LocalizedString, Reference } from './common';
 
+export type StateType =
+  | 'OrderState'
+  | 'LineItemState'
+  | 'ProductState'
+  | 'ReviewState'
+  | 'PaymentState';
+
+export type StateRole = 'ReviewIncludedInStatistics' | 'Return';
+
 export interface State {
   id: string;
   version: number;
   key: string;
-  type: string;
-  roles: any[];
+  type: StateType;
+  roles: StateRole[];
   name: LocalizedString;
   description: LocalizedString;
   builtIn: boolean;
@@ -13,4 +22,14 @@ export interface State {
   initial: boolean;
   createdAt: string;
   lastModifiedAt: string;
+}
+
+export interface StateDraft {
+  key: string;
+  type: StateType;
+  name?: LocalizedString;
+  description?: LocalizedString;
+  initial?: boolean;
+  roles?: StateRole[];
+  transitions: Reference[];
 }
